10b1062cc55cc498a48fdfd65e0236dfb112fa5f
Andrei Zavarin <andrey.zavarin@gmail.com>
2021-05-29 23:10:17 +0300

    Added README

---
+++ README.txt
@@ -0,0 +1,4 @@
+FreeTON version of robonomics contracts. Some architectural changed have also been made.
+
+Install with `npm install`. See scripts folder for interaction utilities. 
+	
--- scripts/config.js
+++ scripts/config.js
@@ -2,6 +2,8 @@ module.exports = {
     toMint : [/*{'to' : '0:a95191fe91f13cb357aaaba6bc1a4aa0479dd67f85d523d2cb4d855c4dd6fcea', 'value' : 500_000_000_000}*/
              ], // XRTs to mint initially
     network : {
+      // Specify 127.0.0.1 if you have TON OS SE instance running locally
+      // see https://docs.ton.dev/86757ecb2/p/641c22-platforms for more info
       endpoints : ["http://195.161.41.59"], // nodes to access the network
       type : "test" // is this test or main network
     }
